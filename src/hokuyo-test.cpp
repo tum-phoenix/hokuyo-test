7c9dcb3ccdda854bef57432ed2f68f6ec02992cd
Hans Kirchner <hans-kirchner@gmx.de>
2017-02-07 09:15:51 +0100

    Fix exit codes

--- src/hokuyo-test.cpp
+++ src/hokuyo-test.cpp
@@ -3,6 +3,12 @@
 #include <iostream>
 
 int main(int argc, char *argv[]) {
+    if(argc != 2) {
+        std::cout << "Usage: hokuyo-test <DEVICE_PATH>\n"
+                  << "  Example: hokuyo-test /dev/ttyACM0";
+        return EXIT_FAILURE;
+    }
+
     qrk::Urg_driver urg;
 
     if (!urg.open(argv[1], 115200)) {
@@ -12,10 +18,12 @@ int main(int argc, char *argv[]) {
 
     if (!urg.set_scanning_parameter(urg.deg2step(-60), urg.deg2step(60), 0)) {
         std::cerr << urg.what() << std::endl;
+        return EXIT_FAILURE;
     }
 
     if (!urg.start_measurement(qrk::Urg_driver::Distance, 1, 0)) {
         std::cerr << urg.what() << std::endl;
+        return EXIT_FAILURE;
     }
 
     std::vector<long> myMeasurement;
@@ -23,9 +31,12 @@ int main(int argc, char *argv[]) {
 
     if (!urg.get_distance(myMeasurement, &time_stamp)) {
         std::cerr << urg.what() << std::endl;
+        return EXIT_FAILURE;
     }
 
     urg.stop_measurement();
 
     urg.close();
+
+    return EXIT_SUCCESS;
 }
